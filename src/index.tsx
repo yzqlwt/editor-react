a7f535315b972efa1c62daa95622b26bc16d2d0b
yzqlwt <yzqlwt@gmail.com>
2021-05-29 18:59:27 +0800

    sync

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,107 @@
-import React from 'react';
-import { render } from 'react-dom';
-import App from './App';
+import ReactDOM from 'react-dom';
+import React, { FC } from 'react';
+import { ReflexContainer, ReflexSplitter, ReflexElement } from 'react-reflex';
+import './index.global.css';
+import Editor from './editor/index';
+interface ReflexStorageDemoProps {}
 
-render(<App />, document.getElementById('root'));
+interface LayoutContainer {
+  LeftContainer: number;
+  CenterContainer: number;
+  RightContainer: number;
+  HierarchyContainer: number;
+  EditorContainer: number;
+}
+class ReflexStorageDemo extends React.Component {
+  layoutState: LayoutContainer;
+
+  constructor(props: FC<ReflexStorageDemoProps>) {
+    super(props);
+    this.onResizePane = this.onResizePane.bind(this);
+    this.layoutState = this.getLayoutState();
+  }
+
+  getLayoutState() {
+    const item = window.localStorage.getItem('layout-flex');
+    if (item) {
+      return JSON.parse(item);
+    }
+    return {
+      LeftContainer: 0.15,
+      CenterContainer: 0.7,
+      RightContainer: 0.15,
+      HierarchyContainer: 0.5,
+      EditorContainer: 0.7,
+    };
+  }
+
+  onResizePane(event) {
+    const { name, flex } = event.component.props;
+    this.layoutState[name].flex = flex;
+    window.localStorage.setItem(
+      'layout-flex',
+      JSON.stringify(this.layoutState)
+    );
+  }
+
+  render() {
+    return (
+      <ReflexContainer orientation="vertical">
+        <ReflexElement flex={this.layoutState.LeftContainer}>
+          <ReflexContainer orientation="horizontal">
+            <ReflexElement
+              flex={this.layoutState.HierarchyContainer}
+              onResize={this.onResizePane}
+              name="hierarchy"
+            >
+              <div className="pane-content">
+                <label>层级管理器</label>
+              </div>
+            </ReflexElement>
+
+            <ReflexSplitter id="gray" />
+
+            <ReflexElement className="bottom-pane">
+              <div className="pane-content">
+                <label>资源管理器</label>
+              </div>
+            </ReflexElement>
+          </ReflexContainer>
+        </ReflexElement>
+        <ReflexSplitter id="gray" />
+        <ReflexElement flex={this.layoutState.CenterContainer}>
+          <ReflexContainer orientation="horizontal">
+            <ReflexElement
+              flex={this.layoutState.EditorContainer}
+              onResize={this.onResizePane}
+              name="editor"
+              propagateDimensionsRate={200}
+              propagateDimensions={true}
+            >
+              <Editor></Editor>
+            </ReflexElement>
+            <ReflexSplitter id="gray" />
+            <ReflexElement className="bottom-pane">
+              <div className="pane-content">
+                <label>组件库</label>
+              </div>
+            </ReflexElement>
+          </ReflexContainer>
+        </ReflexElement>
+        <ReflexSplitter id="gray" />
+        <ReflexElement
+          flex={this.layoutState.RightContainer}
+          onResize={this.onResizePane}
+          className="right-pane"
+          name="inspector"
+        >
+          <div className="pane-content">
+            <label>属性面板</label>
+          </div>
+        </ReflexElement>
+      </ReflexContainer>
+    );
+  }
+}
+
+ReactDOM.render(<ReflexStorageDemo />, document.getElementById('root'));
