de880cf4f06ac17d59b5025741a71e237c50605e
yzqlwt <yzqlwt@gmail.com>
2021-06-16 19:01:50 +0800

    sync

--- src/containers/editor/index.jsx
+++ src/containers/editor/index.jsx
@@ -1,19 +1,15 @@
-import React, { WheelEvent, CSSProperties, MouseEvent } from 'react';
-import Point from '@common/Point';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { calcMinMark } from '@common/utils';
 import Scene from './scene';
 import Ruler from './ruler';
 import Grid from './grid';
-import { calcMinMark } from '@common/utils';
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       scale: 1,
-      transform: {
-        x: 0,
-        y: 0,
-      },
     };
   }
 
@@ -24,11 +20,11 @@ class Editor extends React.Component {
    * @memberof Editor
    */
   handleScroll = (event) => {
-    var curScale = this.state.scale;
-    var delta = event.deltaY;
+    const { scale } = this.state;
+    let delta = event.deltaY;
     delta = delta > 0 ? 1 : -1;
-    delta = delta * (curScale / 7);
-    var targetScale = curScale - delta;
+    delta *= scale / 7;
+    let targetScale = scale - delta;
     targetScale = targetScale < 0.2 ? 0.2 : targetScale;
     targetScale = targetScale > 100 ? 100 : targetScale;
     this.setState({
@@ -38,37 +34,14 @@ class Editor extends React.Component {
 
   handleMouseEvent = (event) => {
     const { buttons } = event;
-    if (buttons == 2) {
-      // this.centerPoint.x += event.movementX;
-      // this.centerPoint.y += event.movementY;
-      // this.setState({
-      //   transform: `translate(${100}px, ${300}px)`
-      // })
-      var editor = document.getElementById('editor').getBoundingClientRect();
-      var scene = document.getElementById('scene').getBoundingClientRect();
-      // console.log(bodyRect)
-      // console.log(canvas_el.left)
-      // console.log(event.pageX)
-      console.log(scene.bottom);
-      this.setState({
-        transform: {
-          x: scene.left + event.movementX - 204.39773559570312,
-          y: scene.top + event.movementY,
-        },
-      });
-      // console.log(document.getElementById('scene'));
-      // console.log(event.pageX);
-      // console.log(`translate(${event.pageX-canvas_el.x}px, ${0}px)`)
-    }
   };
 
   render() {
-    const width = this.props.dimensions.width;
-    const height = this.props.dimensions.height;
-    if (isNaN(width) || isNaN(height)) {
+    const { dimensions } = this.props;
+    const { width, height } = dimensions;
+    if (Number.isNaN(width) || Number.isNaN(height)) {
       return null;
     }
-    console.log(width);
     const { scale } = this.state;
     const gap = calcMinMark(scale);
     return (
@@ -80,12 +53,16 @@ class Editor extends React.Component {
         onWheel={this.handleScroll}
         onMouseMove={this.handleMouseEvent}
       >
-        <Scene scale={scale} gap={gap} width={width} height={height}></Scene>
-        <Ruler scale={scale} gap={gap} width={width} height={height}></Ruler>
-        <Grid scale={scale} gap={gap} width={width} height={height}></Grid>
+        <Scene scale={scale} width={width} height={height} />
+        <Ruler scale={scale} gap={gap} width={width} height={height} />
+        <Grid scale={scale} gap={gap} width={width} height={height} />
       </div>
     );
   }
 }
+
+Editor.propTypes = {
+  dimensions: PropTypes.objectOf(PropTypes.any).isRequired,
+};
 
 export default Editor;
--- src/containers/editor/scene.jsx
+++ src/containers/editor/scene.jsx
@@ -1,5 +1,4 @@
-import React, { WheelEvent, CSSProperties, MouseEvent } from 'react';
-import Point from '@common/Point';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { EditorMargin } from '@common/config';
 
@@ -9,14 +8,52 @@ class Scene extends React.Component {
     this.canvas = React.createRef();
   }
 
+  componentDidMount() {
+    console.log('componentDidMount');
+    const canvas = this.canvas.current;
+    const context = canvas.getContext('2d');
+    context.save();
+  }
+
   componentDidUpdate() {
     const { width, height, scale } = this.props;
     const canvas = this.canvas.current;
     const context = canvas.getContext('2d');
-    context.beginPath();
     context.clearRect(0, 0, width, height);
-    context.fillStyle = "#AAAAAA"; 　
-    context.fillRect(EditorMargin,height-EditorMargin-680*scale,1440*scale+EditorMargin,680*scale);
+    // context.scale(scale, -1 * scale);
+    context.scale(1, -1);
+    context.translate(EditorMargin, -height + EditorMargin);
+    console.log('componentDidUpdate', scale);
+
+    // context.translate(EditorMargin, -height + EditorMargin);
+    context.beginPath();
+    context.fillStyle = '#232323';
+    context.fillRect(100, 100, 100, 50);
+    context.restore();
+    context.save();
+    // this.drawImages();
+  }
+
+  convertX = (x) => {
+    return x + EditorMargin;
+  };
+
+  convertY = (x) => {
+    return x + EditorMargin;
+  };
+
+  drawImages() {
+    const canvas = this.canvas.current;
+    const context = canvas.getContext('2d');
+    const img = new Image();
+    img.src = '../.erb/img/webpack.png';
+    // 当图片准备以后再绘制
+    img.onload = () => {
+      // 绘制图片,按照图片本身的大小进行加载
+      context.drawImage(img, 0, 0);
+      context.restore();
+      context.save();
+    };
   }
 
   render() {
@@ -32,7 +69,6 @@ class Scene extends React.Component {
 Scene.propTypes = {
   width: PropTypes.number.isRequired,
   height: PropTypes.number.isRequired,
-  gap: PropTypes.number.isRequired,
   scale: PropTypes.number.isRequired,
 };
 
